d09c98af22a61753d3f3a86db091a24f44c83e18
Jian-Hua, He <game09116@gmail.com>
2020-05-16 11:13:46 +0200

    Update test

--- concurrency/web.go
+++ concurrency/web.go
@@ -2,11 +2,24 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	Str  string
+	Bool bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChan := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(url string) {
+			resultChan <- result{Str: url, Bool: wc(url)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i += 1 {
+		result := <-resultChan
+		results[result.Str] = result.Bool
 	}
 
 	return results
