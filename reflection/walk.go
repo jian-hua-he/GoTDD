6927917e083f55235b4d35d07b95590c1aa4c71d
Jian-Hua, He <game09116@gmail.com>
2020-05-17 13:30:38 +0200

    Add getValue method

--- reflection/walk.go
+++ reflection/walk.go
@@ -5,10 +5,7 @@ import (
 )
 
 func Walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i += 1 {
 		field := val.Field(i)
@@ -20,4 +17,14 @@ func Walk(x interface{}, fn func(input string)) {
 			Walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
 }
