07d7567afd0bc80fc7345bf63df3de4be483e279
Jian-Hua, He <game09116@gmail.com>
2020-05-17 13:53:19 +0200

    Add new case for array

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -55,6 +55,13 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
+		"arrays": {
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavik"},
+			},
+			[]string{"London", "Reykjavik"},
+		},
 	}
 
 	for n, c := range cases {
