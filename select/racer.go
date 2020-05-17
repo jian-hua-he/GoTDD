3edc0a02c2ac2049d70c30a55fa5de68d72359ca
Jian-Hua, He <game09116@gmail.com>
2020-05-17 10:05:29 +0200

    Make the test passed

--- select/racer.go
+++ select/racer.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"net/http"
+	"time"
+)
+
 func Racer(a, b string) string {
-	return ""
+	startA := time.Now()
+	http.Get(a)
+	aDur := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDur := time.Since(startB)
+
+	if aDur < bDur {
+		return a
+	}
+
+	return b
 }
