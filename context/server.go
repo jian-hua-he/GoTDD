d0e62a340f1955d6943bbac97fbfc496eed697ce
Jian-Hua, He <game09116@gmail.com>
2020-05-19 21:19:12 +0200

    Make the test passed

--- context/server.go
+++ context/server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		data, _ := store.Fetch(r.Context())
+		fmt.Fprintf(w, data)
 	}
 }
