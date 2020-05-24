e0a189ab3cd5e83004742337c7554fd785cafcd5
Jian-Hua, He <game09116@gmail.com>
2020-05-24 23:28:37 +0200

    Fix test

--- prop_base_test/roman_num_test.go
+++ prop_base_test/roman_num_test.go
@@ -15,7 +15,7 @@ func TestRomanNumerals(t *testing.T) {
 	})
 
 	t.Run("2 gets converted to II", func(t *testing.T) {
-		got := ConvertToRoman(1)
+		got := ConvertToRoman(2)
 		want := "II"
 
 		if got != want {
