8cb2b7529df39c6807a6d516a37523a9f4a3e8fc
Jian-Hua, He <game09116@gmail.com>
2020-05-28 23:20:41 +0200

    Add test

--- http_server/player_test.go
+++ http_server/player_test.go
@@ -20,4 +20,18 @@ func TestGETPlayers(t *testing.T) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
+
+	t.Run("returns Floyd’s score", func(test *testing.T) {
+		req, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		resp := httptest.NewRecorder()
+
+		PlayerServer(resp, req)
+
+		got := resp.Body.String()
+		want := "10"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
