ad87cea28a75b5894c215e074a0596d1ab8962c7
Jian-Hua, He <game09116@gmail.com>
2020-05-28 22:57:56 +0200

    Add test

---
+++ http_server/player.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	"net/http"
+)
+
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+}
---
+++ http_server/player_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	t.Run("returns Pepper’s score", func(test *testing.T) {
+		req, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		resp := httptest.NewRecorder()
+
+		PlayerServer(resp, req)
+
+		got := resp.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
