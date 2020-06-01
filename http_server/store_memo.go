a3881c96b4ecd58d42b62868ca3098ab949b7688
Jian-Hua, He <game09116@gmail.com>
2020-06-02 00:07:41 +0200

    Add lock

--- http_server/store_memo.go
+++ http_server/store_memo.go
@@ -1,17 +1,26 @@
 package main
 
+import (
+	"sync"
+)
+
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
+	return &InMemoryPlayerStore{store: map[string]int{}}
 }
 
 type InMemoryPlayerStore struct {
 	store map[string]int
+	mux   sync.Mutex
 }
 
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	i.mux.Lock()
+	defer i.mux.Unlock()
 	return i.store[name]
 }
 
 func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.mux.Lock()
+	defer i.mux.Unlock()
 	i.store[name] += 1
 }
