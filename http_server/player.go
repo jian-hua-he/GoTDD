c1073854e399508334d25a380ae46f3c1728d19b
Jian-Hua, He <game09116@gmail.com>
2020-06-01 23:30:08 +0200

    Make test pass

--- http_server/player.go
+++ http_server/player.go
@@ -11,6 +11,11 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+        w.WriteHeader(http.StatusAccepted)
+        return
+    }
+
     player := strings.TrimPrefix(r.URL.Path, "/players/")
 
     score := p.store.GetPlayerScore(player)
