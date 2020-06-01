fafb2b2e7a8b57e1b9e82a41097e1b97d6329d85
Jian-Hua, He <game09116@gmail.com>
2020-06-02 00:04:49 +0200

    Update main.go

--- http_server/main.go
+++ http_server/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "net/http"
-    "log"
+	"log"
+	"net/http"
 )
 
 func main() {
-    server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
-    if err := http.ListenAndServe(":5000", server); err != nil {
-        log.Fatalf("could not listen on port 5000 %v", err)
-    }
+	if err := http.ListenAndServe(":5000", server); err != nil {
+		log.Fatalf("could not listen on port 5000 %v", err)
+	}
 }
