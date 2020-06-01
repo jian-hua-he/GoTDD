373db9b68b11377fcb93887b9602eb111937aee2
Jian-Hua, He <game09116@gmail.com>
2020-06-01 23:54:20 +0200

    Make test pass

--- http_server/player.go
+++ http_server/player.go
@@ -7,29 +7,30 @@ import (
 )
 
 type PlayerServer struct {
-    store PlayerStore
+	store PlayerStore
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    switch r.Method {
-    case http.MethodPost:
-        p.processWin(w)
-    case http.MethodGet:
-        p.showScore(w, r)
-    }
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, r)
+	case http.MethodGet:
+		p.showScore(w, r)
+	}
 }
 
 func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-    player := strings.TrimPrefix(r.URL.Path, "/players/")
-    score := p.store.GetPlayerScore(player)
-    if score == 0 {
-        w.WriteHeader(http.StatusNotFound)
-    }
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	score := p.store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
 
-    fmt.Fprint(w, score)
+	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter) {
-    p.store.RecordWin("Bob")
-    w.WriteHeader(http.StatusAccepted)
+func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
 }
