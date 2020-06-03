8c9534adf5fc106f420d03ea95d524361fe29125
Jian-Hua, He <game09116@gmail.com>
2020-06-03 20:47:11 +0200

    Add new test

--- http_server/player_test.go
+++ http_server/player_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
@@ -135,6 +136,35 @@ func TestLeague(t *testing.T) {
 		}
 
 		assertStatus(t, resp.Code, http.StatusOK)
+	})
+
+	t.Run("it returns the league table as JSON", func(t *testing.T) {
+		want := []Player{
+			{"Cleo", 32},
+			{"Chris", 20},
+			{"Tiest", 14},
+		}
+
+		store := StubPlayerStore{nil, nil, want}
+		server := NewPlayerServer(&store)
+
+		req, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		resp := httptest.NewRecorder()
+
+		server.ServeHTTP(resp, req)
+
+		var got []Player
+
+		err := json.NewDecoder(resp.Body).Decode(&got)
+		if err != nil {
+			t.Fatalf("Unable to parse response from server %q into slice of Player, %v", resp.Body, err)
+		}
+
+		assertStatus(t, resp.Code, http.StatusOK)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
 	})
 }
 
