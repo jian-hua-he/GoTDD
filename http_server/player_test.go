2ef6dd386e70cb01eedff3e130ec455bd5bce07f
Jian-Hua, He <game09116@gmail.com>
2020-06-06 09:41:44 +0200

    Fix test

--- http_server/player_test.go
+++ http_server/player_test.go
@@ -181,7 +181,7 @@ func TestFileSystemStore(t *testing.T) {
 	t.Run("/league from a reader", func(t *testing.T) {
 		db := strings.NewReader(`[
             {"Name": "Cleo", "Wins": 10},
-            {"Name": "Chris", "Wins": 33},
+            {"Name": "Chris", "Wins": 33}
         ]`)
 
 		store := FileSystemPlayerStore{db}
